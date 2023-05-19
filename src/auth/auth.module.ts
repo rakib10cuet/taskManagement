0d89616591b4c3d7b8bdd9dffb58c7089d21cfaf
rezwoan.rakib <rezwoan.rakib@apsissolutions.com>
2023-05-19 19:09:42 +0600

    change

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { RedisModule } from 'src/redis/redis.module';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '60m' },
     }),
   ],
   controllers: [AuthController],
